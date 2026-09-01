46d7e4a96675b9017263c432d81bc695414be48d
it262174-ux <it262174@trident.ac.jp>
2026-09-01 20:08:32 +0900

    Test CI failure gate

--- test_app.py
+++ test_app.py
@@ -4,5 +4,6 @@ def test_home():
     client = app.test_client()
     response = client.get("/health")
     
-    assert response.status_code == 200
+    assert response.status_code == 500
+    
     
